85d1b972b615f495b6eb9644b8277f2406530156
adimit078 <42521995+adimit078@users.noreply.github.com>
2021-08-09 20:05:36 -0700

    Initial Commit

---
+++ sFlora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sFlora
+//
+//  Created by Aditya Mittal on 8/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
